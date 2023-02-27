e492d20903265dc8002707e58cf32b43413ead61
sil3ntr <richard@spokeculture.com.au>
2023-02-27 21:29:04 +0800

    card design to mimic design from mockup. Trying to use a radio button to be more accessible but not working as I would like

---
+++ script.js
@@ -0,0 +1,11 @@
+const ratingSelect = document.querySelectorAll('.rating');
+    
+    ratingSelect.forEach(element => {
+      element.addEventListener("click", () => {
+        document.querySelector(".select")?.classList.remove("select");
+        element.classList.add("select");
+      });
+    });
+
+const currentSelect = document.querySelectorAll(".select");
+    
