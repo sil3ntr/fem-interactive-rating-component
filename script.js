5b03f5592e9f34752aadc6fe35424714d6b57d6f
sil3ntr <richard@spokeculture.com.au>
2023-02-28 16:08:12 +0800

    swapped out li for buttons for better accessability features

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const ratingSelect = document.querySelectorAll('.rating');
     
     ratingSelect.forEach(element => {
-      element.addEventListener("click", () => {
-        document.querySelector(".select")?.classList.remove("select");
-        element.classList.add("select");
+      element.addEventListener('click', () => {
+        document.querySelector('.select')?.classList.remove('select');
+        element.classList.add('select');
       });
     });
 
-const currentSelect = document.querySelectorAll(".select");
     
